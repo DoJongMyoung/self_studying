6be33243f1d596bf4306df62e639997a9466f66d
Jong Myoung <djm342@naver.com>
2025-02-17 20:00:18 +0900

    v0.3

--- every.py
+++ every.py
@@ -16,3 +16,18 @@
 #
 # print(is_even(8))
 # print(is_even(7))
+
+
+#팔진수 변환 프로그램
+
+def my_oct(num):
+     if num == 0:
+        return ""
+     else:
+        return my_oct(num // 8) + str(num % 8)
+        # str을 나중에 추가하므로 62를 8로 나눈 나머지인 6은 제일 마지막에 왼쪽에 추가될 예정
+        # 62 // 8의 값인 7이 num으로 대입되어서 함수가 다시 실행되고, 7의 나머지인 7은 나중에 추가
+        # 7 // 8의 값인 0이 num으로 대입되는데 이때는 if문에 의해서 ""를 출력함.
+        # 따라서 순차적으로 "" + "7" + "6"이 문자열 형태로 출력됨.
+print(my_oct(62))
+print(type(my_oct(62)))
