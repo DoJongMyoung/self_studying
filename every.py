829e7f9e39a1f92588a2bdc86df955ae25b540d7
Jong Myoung <djm342@naver.com>
2025-02-17 19:34:28 +0900

    v0.2

--- every.py
+++ every.py
@@ -1,29 +1,18 @@
-# numbers=input("숫자를 입력 :").split()
+# a = 11 # 01011
+# b = 30 # 11110
 #
-# num1 = int(numbers[0])
-# num2 = str(numbers[1])
-# num3 = int(numbers[2])
+# print(a & b) #01010 -> 10 // 둘다 1일때만 1
+# print(a ^ b) #10101 -> 21 // 둘이 다를때만 1
+# print(a | b) #11111 -> 31 //하나라도 1이면 1
 #
-# print(f"{num1} , {num2}, {num3}")
-
-course = "       *  1 KEs 2024# KEB !Bootcamp KEB...*!#        "
-# print(course.find('KEB'))
-# print(course.rfind('KEB'))
-# print(course.index('KEB'))
-# print(course.rindex('KEB'))
-# # print(course.find('Inha'))  # -1
-# # print(course.index('Inha'))  # ValueError: substring not found
-# #
-print(course)
-course = course.replace('KEB', 'Inha', 2)
-print(course)
-print(course.strip())
-print(course.strip("!#.*"))
-print(course.strip("!#.* "))
-
-
-# print(course)
-# print(course.replace('KEB', 'Inha'))
-# print(course)
-# course = course.replace('KEB', 'Inha')
-# print(course)
+# def is_even(num):
+#     # if num & 1: #num이 홀수면 T반환
+#     #     print("홀수입니다.")
+#     # else:
+#     #     print("짝수입니다")
+#     return not num & 1 # num & 1 은 num이 홀수이면 T 짝수이면 F 그런데 not이 있으므로 이 함수는 홀수이면 F 짝수이면 T를 반환함.
+#
+#
+#
+# print(is_even(8))
+# print(is_even(7))
