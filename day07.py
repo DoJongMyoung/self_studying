2125ee7e7118c2a2b7728596035bda65aa657908
DoJong <djm342@naver.com>
2025-02-18 21:58:22 +0900

    v0.7

--- day07.py
+++ day07.py
@@ -1,20 +1,29 @@
-#스택 공부
+#링크드 리스트
+#데이터를 담는 부분과 노트를 가르키는 부분으로 이루어져 있음.
 
-class Stack:
+class Node:
+    def __init__(self, data, next=None): #data를 보관할 data변수 // 다음노드의 주소를 찍을 next변수 , 따로 설정하지 않으면 None
+        self.data = data
+        self.next = next
+
+class LinkedList:
     def __init__(self):
-        self.items = []
+        self.head = None
 
-    def push(self,data):
-        self.items.append(data)
+    def append(self,data):
+        if not self.head: # self.head가 None일시 실행
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = Node(data)
 
-    def pop(self):
-        return self.items.pop()
-
-    def size(self):
-        return len(self.items)
-
-    def is_empty(self):
-        return len(self.items) == 0
-
-    def peek(self):
-        return self.items[-1]
+    def search(self, target):
+        current = self.head
+        while current.next:
+            if current.data == target:
+                return True
+            else:
+                current = current.next
+        return False
